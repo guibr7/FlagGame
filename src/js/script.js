38bbc561619edc1b48a2b17f702b2c733bb6134c
Guilherme Henrique <112288334+guibr7@users.noreply.github.com>
2024-02-24 23:37:13 -0300

    Development

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,3 @@
-
 let responseInput = document.querySelector("#responseInput");
 let country;
 let flag;
@@ -6,38 +5,42 @@ let pontuacao = { acertos:0,erros:0 }
 let acerto = document.querySelector("#acerto");
 let erro = document.querySelector("#erro");
 
-
 getCountry()
 
 async function getCountry(){
     const request = await fetch("https://restcountries.com/v3.1/all");
     const ResponseAPI = await request.json();
+    console.log(ResponseAPI)
     displayFlag(ResponseAPI);
 }
 
 const divFlag = document.querySelector("#flag")
 
 function displayFlag(ResponseAPI){
-
-    country = ResponseAPI[Math.floor(Math.random()*250)];
-    console.log(country.translations.por.common);
-    languageCountry = country.translations.por.common;
-    flag = country.flags.png;
-    divFlag.setAttribute("src",flag);
+  country = ResponseAPI[Math.floor(Math.random()*250)];
+  console.log(country.translations.por.common);
+  languageCountry = (country.translations.por.common).toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+  flag = country.flags.png;
+  divFlag.setAttribute("src",flag);
 }
 
 document.querySelector("#form").addEventListener("submit",function(event){
-  if(responseInput.value.toLowerCase() === languageCountry.toLowerCase()){
-    confirm('correto');
-    pontuacao.acertos+=1
-    acerto.innerHTML = pontuacao.acertos
-    getCountry();
+
+  let responsev1  = responseInput.value.trim().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+
+  if(responsev1 == languageCountry){
     responseInput.value = ''
+    confirm('correto');
+    pontuacao.acertos+=1;
+    acerto.innerHTML = pontuacao.acertos;
+    getCountry();
+  } else {
+    responseInput.value = country.translations.por.common
   }
  })
 
  document.querySelector("#skipButton").addEventListener("click",(event)=>{
-    event.this
+    responseInput.value = ''
     pontuacao.erros+=1
     erro.innerHTML = pontuacao.erros
     getCountry()
